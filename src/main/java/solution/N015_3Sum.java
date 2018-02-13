d08e1b6e1d6d47d6daeefa2ef755fd7c6c0181e1
lishiqian <2931164745@qq.com>
2018-02-13 20:03:12 +0800

    3Sum AC

--- src/main/java/solution/N015_3Sum.java
+++ src/main/java/solution/N015_3Sum.java
@@ -1,6 +1,7 @@
 package solution;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 /**
  * Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
@@ -22,16 +23,22 @@ import java.util.List;
  */
 public class N015_3Sum {
     public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
         ArrayList<List<Integer>> resultList = new ArrayList<List<Integer>>();
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                for (int k = j+1; k < nums.length; k++) {
-                    if(nums[i] + nums[j] + nums[k] == 0);
-                    ArrayList<Integer> resultItem = new ArrayList<Integer>(3);
-                    resultItem.add(nums[i]);
-                    resultItem.add(nums[j]);
-                    resultItem.add(nums[k]);
-                    resultList.add(resultItem);
+        for (int i = 0; i < nums.length-2; i++) {
+            if(i == 0 || (i > 0 && nums[i] != nums[i-1])){
+                int sum = 0 - nums[i];
+                int low = i + 1, high = nums.length - 1;
+                while (low < high){
+                    if(nums[low] + nums[high] == sum){
+                        resultList.add(Arrays.asList(nums[i],nums[low],nums[high]));
+                        while (low < high && nums[low] == nums[low + 1]) low++;
+                        while (low < high && nums[high] == nums[high - 1]) high--;
+                        low++;
+                        high--;
+                    }else if(nums[low] + nums[high] < sum){
+                        low ++;
+                    }else high--;
                 }
             }
         }
