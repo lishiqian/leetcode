de93fb9942a915f674b50e7198154da9648f8f90
lishiqian <lishiqian@changingedu.com>
2018-02-22 15:46:10 +0800

    新加递归解法

--- src/main/java/solution/N021_MergeTwoSortedLists.java
+++ src/main/java/solution/N021_MergeTwoSortedLists.java
@@ -16,22 +16,37 @@ import common.ListNode;
  * 归并两个排序链表,归并后依然有序
  */
 public class N021_MergeTwoSortedLists {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode start = new ListNode(0);
-        ListNode current = start;
-        while (l1 != null && l2 != null){
-            if(l1.val < l2.val){
-                current.next = l1;
-                l1 = l1.next;
-            }else{
-                current.next = l2;
-                l2 = l2.next;
-            }
-            current = current.next;
-        }
+    //循环
+//    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+//        ListNode start = new ListNode(0);
+//        ListNode current = start;
+//        while (l1 != null && l2 != null){
+//            if(l1.val < l2.val){
+//                current.next = l1;
+//                l1 = l1.next;
+//            }else{
+//                current.next = l2;
+//                l2 = l2.next;
+//            }
+//            current = current.next;
+//        }
+//
+//        if(l1 != null) current.next = l1;
+//        if(l2 != null) current.next = l2;
+//        return start.next;
+//    }
 
-        if(l1 != null) current.next = l1;
-        if(l2 != null) current.next = l2;
-        return start.next;
+    //递归
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        }else{
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
     }
 }
