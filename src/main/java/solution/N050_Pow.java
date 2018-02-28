618ea8c5236fea2c18e7a8951e75ff63aadc0a96
lishiqian <lishiqian@changingedu.com>
2018-02-28 17:00:41 +0800

    新增

---
+++ src/main/java/solution/N050_Pow.java
@@ -0,0 +1,39 @@
+package solution;
+
+import org.junit.Test;
+
+/**
+ * Implement pow(x, n).
+ * Example 1:
+ *
+ * Input: 2.00000, 10
+ * Output: 1024.00000
+ * Example 2:
+ *
+ * Input: 2.10000, 3
+ * Output: 9.26100
+ */
+public class N050_Pow {
+    public double myPow(double x, int n) {
+        double result = 1;
+        long N = n;
+        if(n < 0) {
+            x = 1 / x;
+            N = -n;
+        }
+
+        while (N > 0){
+            if((N & 1) == 1){
+                result *= x;
+            }
+            x *= x;
+            N = N >> 1;
+        }
+        return result;
+    }
+
+    @Test
+    public void test(){
+        System.out.println(myPow(5,1));
+    }
+}
