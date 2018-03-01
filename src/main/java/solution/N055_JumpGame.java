36c8df8678662ec9dcd3239573db4c49471c49be
lishiqian <lishiqian@changingedu.com>
2018-03-01 10:35:02 +0800

    新增

---
+++ src/main/java/solution/N055_JumpGame.java
@@ -0,0 +1,24 @@
+package solution;
+
+/**
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+ *
+ * Each element in the array represents your maximum jump length at that position.
+ *
+ * Determine if you are able to reach the last index.
+ *
+ * For example:
+ * A = [2,3,1,1,4], return true.
+ *
+ * A = [3,2,1,0,4], return false.
+ */
+public class N055_JumpGame {
+
+    public boolean canJump(int[] nums) {
+        int rach = 0;
+        for (int i = 0; i < nums.length && i <= rach; i++) {
+            rach = Math.max(rach,nums[i]+i);
+        }
+        return rach >= nums.length - 1;
+    }
+}
