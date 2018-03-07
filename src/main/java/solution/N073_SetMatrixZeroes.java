6552922448ed003aa3bcc8304c4645f34c761edf
lishiqian <lishiqian@changingedu.com>
2018-03-07 17:01:58 +0800

    添加 空间O(1)解法

--- src/main/java/solution/N073_SetMatrixZeroes.java
+++ src/main/java/solution/N073_SetMatrixZeroes.java
@@ -12,34 +12,99 @@ import java.util.Iterator;
  * 给你一个矩阵，把其中包含0的元素的行和列变成0
  */
 public class N073_SetMatrixZeroes {
-    public void setZeroes(int[][] matrix) {
-        HashSet<Integer> rowSet = new HashSet<Integer>();
-        HashSet<Integer> colSet = new HashSet<Integer>();
+//    public void setZeroes(int[][] matrix) {
+//        HashSet<Integer> rowSet = new HashSet<Integer>();
+//        HashSet<Integer> colSet = new HashSet<Integer>();
+//
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                if(matrix[i][j] == 0){
+//                    rowSet.add(i);
+//                    colSet.add(j);
+//                }
+//            }
+//        }
+//
+//
+//        Iterator<Integer> colIt = colSet.iterator();
+//        while (colIt.hasNext()){
+//            int col = colIt.next();
+//            for (int i = 0; i < matrix.length; i++) {
+//                matrix[i][col] = 0;
+//            }
+//        }
+//
+//        Iterator<Integer> rowIt = rowSet.iterator();
+//        while (rowIt.hasNext()){
+//            int row = rowIt.next();
+//            for (int i = 0; i < matrix[row].length; i++) {
+//                matrix[row][i] = 0;
+//            }
+//        }
+//    }
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
+
+    //O(1)空间复杂度解放
+    public void setZeroes(int[][] matrix) {
+        boolean firstRowExistZero = false;  
+        boolean firstColExistZero = false;
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i = 0; i < n; i++) {
+            if(matrix[0][i] == 0){
+                firstRowExistZero = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            if(matrix[i][0] == 0){
+                firstColExistZero = true;
+                break;
+            }
+        }
+
+
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 4; j < n; j++) {
                 if(matrix[i][j] == 0){
-                    rowSet.add(i);
-                    colSet.add(j);
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-
-        Iterator<Integer> colIt = colSet.iterator();
-        while (colIt.hasNext()){
-            int col = colIt.next();
-            for (int i = 0; i < matrix.length; i++) {
-                matrix[i][col] = 0;
-            }
+        for (int i = 1; i < m; i++) {
+            if(matrix[i][0] == 0)
+                fullRowZero(matrix,i,m,n);
         }
 
-        Iterator<Integer> rowIt = rowSet.iterator();
-        while (rowIt.hasNext()){
-            int row = rowIt.next();
-            for (int i = 0; i < matrix[row].length; i++) {
-                matrix[row][i] = 0;
-            }
+        for (int j = 1; j < n; j++) {
+            if(matrix[0][j] == 0)
+                fullColZero(matrix,j,m,n);
+        }
+
+        if(firstRowExistZero){
+            fullRowZero(matrix,0,m,n);
+        }
+
+        if(firstColExistZero){
+            fullColZero(matrix,0,m,n);
+        }
+    }
+    
+    public void fullRowZero(int[][] matrix,int i,int m,int n){
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+
+    public void fullColZero(int[][] matrix,int j,int m,int n){
+        for (int i = 0; i < m; i++) {
+            matrix[i][j] = 0;
         }
     }
 }
