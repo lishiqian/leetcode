fef8972a10972b87662eaef0efb8ebd90eda2165
Administrator <2931164745@qq.com>
2018-04-01 14:27:24 +0800

    新增加

---
+++ src/main/java/solution/N150_EvaluateReversePolishNotation.java
@@ -0,0 +1,49 @@
+package solution;
+
+import java.util.Stack;
+
+/**
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+ *
+ * Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+ *
+ * Some examples:
+ * ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+ * ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+ */
+public class N150_EvaluateReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int num1 = 0;
+        int num2 = 0;
+        for (int i = 0; i < tokens.length; i++) {
+            switch (tokens[i]){
+                case "+":
+                    num2 = stack.pop();
+                    num1 = stack.pop();
+                    stack.push(num1 + num2);
+                    break;
+                case "-":
+                    num2 = stack.pop();
+                    num1 = stack.pop();
+                    stack.push(num1 - num2);
+                    break;
+                case "*":
+                    num2 = stack.pop();
+                    num1 = stack.pop();
+                    stack.push(num1 * num2);
+                    break;
+                case "/":
+                    num2 = stack.pop();
+                    num1 = stack.pop();
+                    stack.push(num1 / num2);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(tokens[i]));
+                    break;
+            }
+        }
+
+        return stack.pop();
+    }
+}
